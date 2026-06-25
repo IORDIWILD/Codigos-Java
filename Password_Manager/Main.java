88c13e65dbed72c261813bba77a388e54a8e32e4
Iordiban <geekaguilera@gmail.com>
2026-06-24 23:31:27 -0600

    segundo commit

--- Password_Manager/Main.java
+++ Password_Manager/Main.java
@@ -1,6 +1,5 @@
 import java.util.*;
 
-
 public class Main{
 public static void main(String args[]){
 Scanner in = new Scanner(System.in);
@@ -18,11 +17,14 @@ while(menu){
 System.out.println("\n=== MENÚ ===");
 System.out.println("1. Agregar contraseña");
 System.out.println("2. Mostrar todas");
-System.out.println("3. Salir");
+System.out.println("3. Borrar Password");
+System.out.println("4. Modificar Password");
+System.out.println("5. Guardar Password");
+System.out.println("6. Salir");
 System.out.print("Seleccione una opción: ");
 
 menu_case= in.nextInt();
-
+    System.out.println();
 switch(menu_case){
 
 case 1:{    
@@ -36,11 +38,95 @@ case 1:{
     manager.addPassword(e);
     break;
 }
-case 2:{    
+case 2:{
+    System.out.println();    
     manager.showPasswords();
     break;
 }
-case 3:{    
+case 3:{
+    System.out.println("Estas son las Urls existentes :");
+    for(int i=0; i<manager.numdePassword(); i++){
+        System.out.println("Url #" + (i+1) + ": " + manager.getPassword(i).getUrl());
+    }
+    System.out.println("Ingrese el numero de la que desee eliminar :");
+    int numElim = in.nextInt();
+    if(0<numElim && numElim<=manager.numdePassword())
+        manager.deletePassword(numElim);
+    else
+        System.out.println("Numero Invalido");
+    System.out.println();
+    break;
+
+}
+case 4:{
+    System.out.println();
+    System.out.println("Estas son las Urls existentes :");
+    for(int i=0; i<manager.numdePassword(); i++){
+        System.out.println("Url #" + (i+1) + ": " + manager.getPassword(i).getUrl());
+    }
+    System.out.println("Ingrese el Numero de Password que desee modificar:");
+    int numModif = in.nextInt();
+    PasswordEntry e = manager.getPassword(numModif-1);
+    System.out.println("Ingrese el numero acorde de lo que quiera modificar del Password #" +numModif +":");
+    System.out.println("1. Modificar Url");
+    System.out.println("2. Modificar Password");
+    System.out.println("3. Modificar User");
+    System.out.println("4. Modificar Notas");
+    System.out.println("5. Modificar Todo"); 
+    numModif = in.nextInt();
+    in.nextLine();
+    switch(numModif){
+        case 1:{
+            System.out.println("Ingrese una nueva Url:");
+            String nuevaUrl = in.nextLine();
+            e.setUrl(nuevaUrl);
+            System.out.println("Password Modificada Exitosamente");
+            break;
+        }
+        case 2:{
+            System.out.println("Ingrese una nueva Password:");
+            String nuevaPassword = in.nextLine();
+            e.setPassword(nuevaPassword);
+            System.out.println("Password Modificada Exitosamente");
+            break;
+        }
+        case 3:{
+            System.out.println("Ingrese un nuevo User:");
+            String nuevoUser = in.nextLine();
+            e.setUser(nuevoUser);
+            System.out.println("Password Modificada Exitosamente");
+            break;
+        }
+        case 4:{
+            System.out.println("Ingrese una nueva Nota:");
+            String nuevaNota = in.nextLine();
+            e.setNotas(nuevaNota);
+            System.out.println("Password Modificada Exitosamente");
+            break;
+        }
+        case 5:{
+            System.out.println("Escriba la Url, Contrasenia, User y las Notas en el orden en que se mencionaron ");
+            String nuevaUrl = in.nextLine();
+            String nuevaPassword = in.nextLine();
+            String nuevoUser = in.nextLine();
+            String nuevaNota = in.nextLine();
+            e.setUrl(nuevaUrl);
+            e.setPassword(nuevaPassword);
+            e.setUser(nuevoUser);
+            e.setNotas(nuevaNota);
+            System.out.println("Password Modificada Exitosamente");
+
+            break;
+        }       
+
+    }
+    break;
+}
+case 5:{
+    manager.guardarArchivo();
+    break;
+}
+case 6:{    
 
     menu = false;
     break;
--- Password_Manager/PasswordManager.java
+++ Password_Manager/PasswordManager.java
@@ -1,8 +1,46 @@
 import java.util.*;
+import java.io.*;
+
+
 
 
 public class PasswordManager{
 private ArrayList<PasswordEntry> passwordList;
+
+
+
+public void guardarArchivo(){
+    try(FileWriter fw = new FileWriter("passwords.txt")){
+        for(int i = 0; i<passwordList.size(); i++){
+            fw.write(passwordList.get(i).getUrl()+"|"+passwordList.get(i).getPassword()+"|"+passwordList.get(i).getUser()+"|"+passwordList.get(i).getNotas()+"|");
+            fw.write("\n");
+        }
+
+    }
+    catch(IOException e){
+
+         e.printStackTrace();
+
+    }
+}
+
+public PasswordManager cargarArchivo(PasswordManager e){
+    try(BufferedReader bf = new BufferedReader(new FileReader("passwords.txt"))){
+        String linea;
+        while((linea = bf.readLine()) != null){
+            String [] bfPassword = linea.split("\\|");
+            passwordList.add(new PasswordEntry(bfPassword[0], bfPassword[1], bfPassword[2], bfPassword[3]));
+        }
+        return e;
+    }
+    catch(IOException c){
+
+        System.out.println("Error al leer");
+        return null;
+    }
+    
+}
+
 
 public PasswordManager() {
         passwordList = new ArrayList<PasswordEntry>();
@@ -20,10 +58,29 @@ if(passwordList.isEmpty()){
 }
 else{
     for(int i = 0 ; i< passwordList.size(); i++){
+        System.out.println("Password #"+ (i+1));
         passwordList.get(i).mostrar();
     }
 }
-
-
 }
+
+public int numdePassword(){
+    int num=0;
+    for(int i=0; i<passwordList.size(); i++){
+        num++;
+    }
+
+    return num;
+}
+
+public void deletePassword(int index){
+
+    passwordList.remove(index-1);
+    System.out.println("Contraseña borrada exitosamente!");
+}
+
+public PasswordEntry getPassword(int index){
+    return passwordList.get(index);
+}
+
 }
